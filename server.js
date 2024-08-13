b887a3cdce422a2147e7f7c176defdda39db880b
elbekjustin <elbekjumanazarov03@gmail.com>
2024-08-14 00:22:39 +0900

    BRR:buil dexpress web server

---
+++ server.js
@@ -0,0 +1,30 @@
+console.log("Web Serverni boshlash");
+const express = require("express");
+const app = express();
+const http = require("http");
+
+    //1. Kirish code
+    app.use(express.static("public"));
+    app.use(express.json());
+    app.use(express.urlencoded({extended: true}));
+
+    //2. Session code
+
+    //3. Views code
+    app.set("views", "views");
+    app.set("view engine", "ejs");
+
+    //4. Routing code
+    app.get("/hello", function (req, res) {
+        res.end(`<h1 style="color:red">Hello World by ElbekAli</h1>`);
+    });
+
+    app.get("/gift", function (req, res) {
+        res.end(`<h1 style="color:orenge">Siz sovg'alar bo'limidasiz.</h1>`);
+    });
+
+    const server = http.createServer(app);
+    let PORT = 3000;
+    server.listen(PORT, function () {
+        console.log(`The server is running successfully on port: ${PORT}`);
+    });
