915041fdd28b0d7e6067a403b44a2b952c74b0b3
elbekjustin <elbekjumanazarov03@gmail.com>
2024-08-14 17:37:42 +0900

    Kechagi kodlarni to'g'irladim

--- server.js
+++ server.js
@@ -15,13 +15,27 @@ const http = require("http");
     app.set("view engine", "ejs");
 
     //4. Routing code
-    app.get("/hello", function (req, res) {
-        res.end(`<h1 style="color:red">Hello World by ElbekAli</h1>`);
+    app.post("/create-item", (req, res) => {
+        console.log(req.body);
+        res.json({test: "success"});
     });
 
-    app.get("/gift", function (req, res) {
-        res.end(`<h1 style="color:orenge">Siz sovg'alar bo'limidasiz.</h1>`);
+    app.get("/", function(req, res) {
+        res.render('harid');
     });
+
+
+
+    // app.get("/hello", function (req, res) {
+    //     res.end(`<h1 style="color:red">Hello World by ElbekAli</h1>`);
+    // });
+
+    // app.get("/gift", function (req, res) {
+    //     res.end(`<h1 style="color:orenge">Siz sovg'alar bo'limidasiz.</h1>`);
+    // });
+
+
+
 
     const server = http.createServer(app);
     let PORT = 3000;
