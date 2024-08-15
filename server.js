2ba793810b8ae03a9033e5e9384265f923029e9b
elbekjustin <elbekjumanazarov03@gmail.com>
2024-08-15 17:20:48 +0900

    feat:develop author router

--- server.js
+++ server.js
@@ -2,6 +2,16 @@ console.log("Web Serverni boshlash");
 const express = require("express");
 const app = express();
 const http = require("http");
+const fs = require("fs");
+
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+    if(err) {
+        console.log("ERROR:", err);
+    } else {
+        user=JSON.parse(data)
+    }
+});
 
     //1. Kirish code
     app.use(express.static("public"));
@@ -19,6 +29,10 @@ const http = require("http");
         console.log(req.body);
         res.json({test: "success"});
     });
+
+    app.get('/author', (req, res) => {
+        res.render("author", {user: user });
+    })
 
     app.get("/", function(req, res) {
         res.render('harid');
