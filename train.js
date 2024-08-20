608e5f1baa87a5d072cbe0a1731a13ec63ac5858
elbekjustin <elbekjumanazarov03@gmail.com>
2024-08-21 07:37:48 +0900

    feat: A_TASK masala

--- train.js
+++ train.js
@@ -48,43 +48,43 @@
 
 
 
-// 22-dars
+// // 22-dars
 
-console.log("Jack Ma maslahatlari");
-const list = [
-    "Yaxshi talaba bo'ling", // 0-20
-    "To'g'ri boshliq tanlang va ko'proq xato qiling", // 20-30
-    "O'zingizning ishlaringizni boshlang boshlang", // 30-40
-    "Siz kuchli bo'lgan narsalarni qiling", // 40-50
-    "Yoshlarga investitsiya qiling", //50-60
-    "Endi biroz dam oling va sekinlashgan holda davom eting", // 60- ~
-];
+// console.log("Jack Ma maslahatlari");
+// const list = [
+//     "Yaxshi talaba bo'ling", // 0-20
+//     "To'g'ri boshliq tanlang va ko'proq xato qiling", // 20-30
+//     "O'zingizning ishlaringizni boshlang boshlang", // 30-40
+//     "Siz kuchli bo'lgan narsalarni qiling", // 40-50
+//     "Yoshlarga investitsiya qiling", //50-60
+//     "Endi biroz dam oling va sekinlashgan holda davom eting", // 60- ~
+// ];
 
-function maslahatBering(a, callback) {
-    if(typeof a !== "number") callback("insert a number", null);
-    // if(typeof a !== "number", a < 0) callback("insert a number", null);
-    else if (a > 0 && a <= 20) callback(null, list[0]);
-    else if (a > 20 && a <= 30) callback(null, list[1]);
-    else if (a > 30 && a <= 40) callback(null, list[2]);
-    else if (a > 40 && a <= 50) callback(null, list[3]);
-    else if (a > 50 && a <= 60) callback(null, list[4]);
-     else { 
-        setInterval(function () {
-            callback(null, list[5]);
-        },1000);
+// function maslahatBering(a, callback) {
+//     if(typeof a !== "number") callback("insert a number", null);
+//     // if(typeof a !== "number", a < 0) callback("insert a number", null);
+//     else if (a > 0 && a <= 20) callback(null, list[0]);
+//     else if (a > 20 && a <= 30) callback(null, list[1]);
+//     else if (a > 30 && a <= 40) callback(null, list[2]);
+//     else if (a > 40 && a <= 50) callback(null, list[3]);
+//     else if (a > 50 && a <= 60) callback(null, list[4]);
+//      else { 
+//         setInterval(function () {
+//             callback(null, list[5]);
+//         },1000);
        
-    }
-};
+//     }
+// };
 
-console.log('passed here 0');
+// console.log('passed here 0');
 
-maslahatBering(88, (err, data) => {
-    if(err) console.log("ERROR:", err);
-    else {
-        console.log("javob:", data);
-    }
-});
-console.log('passed here 1');
+// maslahatBering(88, (err, data) => {
+//     if(err) console.log("ERROR:", err);
+//     else {
+//         console.log("javob:", data);
+//     }
+// });
+// console.log('passed here 1');
 
 
 
@@ -136,7 +136,7 @@ console.log('passed here 1');
 
    
 
-
+// Call via then/catch
 // console.log('passed here 0');
 
 // maslahatBering(30)
@@ -168,4 +168,20 @@ console.log('passed here 1');
 // });
 // console.log('passed here 1');
 
-
+function countLetter(letter, word) {
+    letter = letter.toLowerCase();
+    word = word.toLowerCase();
+    let count = 0;
+  
+    for (let i = 0; i < word.length; i++) {
+      if (word[i] === letter) {
+        count++;
+      }
+    }
+  
+    return count;
+  }
+  
+//   console.log(countLetter("k", "Koreya")); 
+console.log(countLetter("r", "Uzbekistan")); 
+  
