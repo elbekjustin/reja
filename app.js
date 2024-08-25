c7fd9bd84e9c03d5a965d9619e88d8ab9f665bdf
elbekjustin <elbekjumanazarov03@gmail.com>
2024-08-25 16:02:32 +0900

    FrontEnd javascriptni qurish

--- app.js
+++ app.js
@@ -31,15 +31,18 @@ const db = require("./server").db();
     app.post("/create-item", (req, res) => {
         console.log("user entered /create-item");
 
-        console.log(req.body);
+        // console.log(req.body);
         const new_reja = req.body.reja;
         db.collection("plans").insertOne({reja: new_reja}, (err, data) => {
-            if (err) {
-                console.log(err);
-                res.end("something went wrong");
-            } else {
-                res.end("successfully addres");
-            }
+        
+            res.json(data.ops[0]);
+         
+            // if (err) {
+            //     console.log(err);
+            //     res.end("something went wrong");
+            // } else {
+            //     res.end("successfully addres");
+            // }
         });
     });
 
