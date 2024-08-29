b09680acbf5283ac8e428a014e0eb30884c5e966
elbekjustin <elbekjumanazarov03@gmail.com>
2024-08-30 01:22:07 +0900

    fix: Port 7003

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ mongodb.connect(connectionString, {
         module.exports =client
         const app = require("./app");
         const server = http.createServer(app);
-        let PORT = 3000;
+        let PORT = 7003;
         server.listen(PORT, function () {
             console.log(`The server is running successfully on port: ${PORT}, http://localhost:${PORT}`
     
