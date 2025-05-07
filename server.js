a96c145a3af17f4660f24c6241dd50e1c7db6e7d
elbekjustin <elbekjumanazarov03@gmail.com>
2025-05-08 04:47:18 +0900

    fix: port 4000 => 5000

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ mongodb.connect(connectionString, {
         module.exports =c
         const app = require("./app");
         const server = http.createServer(app);
-        let PORT = 4000;
+        let PORT = 5000;
         server.listen(PORT, function () {
             console.log(`The server is running successfully on port: ${PORT}, http://localhost:${PORT}`
     
